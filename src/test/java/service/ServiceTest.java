1feaf6867709905db635bdfe21daeb00aff59d88
Bektursun.Kadyrkulov <Bektursun.Kadyrkulov@x5.ru>
2021-03-01 22:15:45 +0300

    Ready first HM

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -2,17 +2,11 @@ package service;
 
 import entity.Task;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.util.Assert;
 import service.reader.ReaderImpl;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ServiceTest {
 
